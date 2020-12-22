79747984061376a72fcee91be9644d5a5090c59e
kevin.sie <kevin.sie@mtaxi.com.tw>
2020-12-22 17:29:56 +0800

    Initial Commit

---
+++ Shared/swiftUItestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUItestApp.swift
+//  Shared
+//
+//  Created by user on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUItestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
